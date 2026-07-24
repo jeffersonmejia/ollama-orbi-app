8ef1f8a16ebcf0ee4bb09157a0f9ae01955a79a2
Jefferson Mejia <jeffersonmejiach01@gmail.com>
2026-07-23 22:01:25 -0500

    Revert "feat: add local Whisper and Kokoro voice mode"
    This reverts commit 5092a90846d1470e51200ceec387f9824838751f.

--- Program.cs
+++ Program.cs
@@ -30,12 +30,6 @@ builder.Services.AddHttpClient<OllamaProductService>(client =>
     client.BaseAddress = new Uri(baseUrl.TrimEnd('/') + "/");
     client.Timeout = TimeSpan.FromSeconds(45);
 });
-builder.Services.AddHttpClient<LocalVoiceService>(client =>
-{
-    var baseUrl = builder.Configuration["Voice:BaseUrl"] ?? "http://localhost:8000";
-    client.BaseAddress = new Uri(baseUrl.TrimEnd('/') + "/");
-    client.Timeout = TimeSpan.FromMinutes(3);
-});
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
